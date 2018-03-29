289444bef9a9fbd001e1b9cbbf1086f59eb8ea1d
Colin Fike <colin.fike@gmail.com>
2018-03-29 16:11:38 -0700

    Add remote debugging requirement.

--- main_template.py
+++ main_template.py
@@ -3,3 +3,11 @@ This module is the main module in the template.
 
 More information here.
 """
+# For remote debugging
+# Don't forget to expose the ports on the docker container.
+# -p 127.0.0.1:3000:3000
+
+# import ptvsd
+# ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3000))
+# ptvsd.wait_for_attach()
+# ptvsd.break_into_debugger()
