d84feff80e65d4d4c63f5c3a0fe2b53ff811546f
Colin Fike <colin.fike@gmail.com>
2018-04-23 12:07:47 -0700

    Update template.

--- main_template.py
+++ main_template.py
@@ -7,7 +7,7 @@ More information here.
 # Don't forget to expose the ports on the docker container.
 # -p 127.0.0.1:3000:3000
 
-# import ptvsd
-# ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3000))
-# ptvsd.wait_for_attach()
-# ptvsd.break_into_debugger()
+# from easy_ptvsd import wait_and_break
+# @wait_and_break()
+# def test_func():
+#     ...
